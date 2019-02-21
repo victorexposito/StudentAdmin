a520e504e8a41a6cd2a9e174591e78a2efee3dd1
victor <victorexposito@hotmail.dk>
2019-02-21 18:07:18 +0100

    Service Victor

---
+++ src/dk/kea/studentAdmin/service/ExamService.java
@@ -0,0 +1,72 @@
+package dk.kea.studentAdmin.service;
+
+import dk.kea.studentAdmin.model.Exam;
+import dk.kea.studentAdmin.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExamService {
+    /*
+    The instance. There can be only one.
+     */
+    private static ExamService mySelf = new ExamService();
+
+    /*
+    The list of student
+     */
+    private List<Exam> exams = new ArrayList<>();
+
+    /*
+    private constructor to avoid creation from outside the class
+     */
+
+    private ExamService() {
+    }
+
+    /*
+    Return the one and only instance
+     */
+    public static ExamService getExamsService() {
+        return mySelf;
+    }
+
+    /**
+     * add a student
+     * @param// student
+     * @return
+     */
+    public boolean addExams(Exam exam) {
+        return exams.add(exam);
+    }
+
+    /**
+     * remove s from student list - list methods for finding an object relies on equals method !!!
+     * @param// t
+     * @return
+     */
+    public boolean removeStudent(Exam e) {
+
+        return exams.remove(e);
+    }
+
+    /**
+     *
+     * @return the student list. Only for viewing
+     */
+    public List<Exam> getExamList() {
+        return new ArrayList<>(exams);
+    }
+
+    /**
+     *
+     * Return a specific student based on the indentifier( to be determined)
+     * @param identifier (rename)
+     * @return
+     */
+    public Exam getBySomeIndentifier(Object identifier) {
+        //TODO implement
+        return null;
+    }
+
+}
